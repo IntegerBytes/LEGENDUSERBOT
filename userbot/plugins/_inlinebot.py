5fb6c1ac58fcc9e81f1ffa57e683a708b29b9d9b
†hê~Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-09-30 13:08:59 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -431,7 +431,7 @@ if Config.BOT_USERNAME is not None and tgbot is not None:
         else:
             if not CMD_HELP_BOT[cmd]["info"]["warning"] == "":
                 result += f"**⚠️ 𝚆𝚊𝚛𝚗𝚒𝚗𝚐 :**  {CMD_HELP_BOT[cmd]['info']['warning']}\n"
-            result += f"**🔸️ Type :** {CMD_HELP_BOT[cmd]['info']['type']}\n"
+            result += f"**⚡ Type :** {CMD_HELP_BOT[cmd]['info']['type']}\n"
             result += f"**ℹ️ 𝙸𝚗𝚏𝚘 :**  {CMD_HELP_BOT[cmd]['info']['info']}\n"
         command = CMD_HELP_BOT[cmd]["commands"][commands]
         if command["params"] is None:
